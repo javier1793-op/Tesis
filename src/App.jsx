935f9a91c667d0122b00cf6da9be82e901cbc177
NicolasAyal <javier17utn@gmail.com>
2023-01-09 21:03:06 -0300

    inicio login

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./Pages/Login"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Login/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './login.scss'
+
+const Login = () => {
+  return (
+    <>
+       <div className="conteiner">
+        <div className="box"> 
+            <div className="cover"></div>
+            <div className="shadow"></div>
+            <div className="content"></div>
+            
+        </div>
+       </div>
+    </>
+  )
+}
+
+export default Login
