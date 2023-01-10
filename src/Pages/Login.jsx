3fe0db87705629d37b6e21dfc98a8231ca686d24
NicolasAyal <javier17utn@gmail.com>
2023-01-10 12:17:44 -0300

    finished login design

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './login.scss'
 
+
 const Login = () => {
   return (
     <>
@@ -8,7 +9,30 @@ const Login = () => {
         <div className="box"> 
             <div className="cover"></div>
             <div className="shadow"></div>
-            <div className="content"></div>
+            <div className="content">
+              <div className="form">
+                <h3 className="logo"><i class="fa-solid fa-door-open"></i></h3>
+                <h2>Sing in</h2>
+                <div className="inputBox">
+                  <input type="text" required />
+                  <i class="fa-solid fa-user"></i>
+                  <span>Username</span>
+                </div>
+                <div className="inputBox">
+                  <input type="password" required />
+                  <i class="fa-solid fa-key"></i>
+                  <span>Password</span>
+                </div>
+                <div className="links">
+                
+                  <a href="#">Forgot Password </a>
+                  <i class="fa-solid fa-question"></i>
+                </div>
+                <div className="inputBox">
+                  <input type="submit" value="Login" />
+                </div>
+              </div>
+            </div>
             
         </div>
        </div>
